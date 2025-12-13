54c2ab4f59ad1d15d7638cc324db65c6bda6a440
d2tk <inv3theories@gmail.com>
2025-12-13 22:59:35 +0900

    add articles section

---
+++ components/Scanline.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Scanline: React.FC = () => {
+    return (
+        <div className="fixed inset-0 w-full h-full scanline z-40 pointer-events-none"></div>
+    );
+};
+
+export default Scanline;
