55c1d006d3be743fb09a7e32a587019f8ec46353
D2TK <neostrategy@gmail.com>
2026-01-12 00:51:46 +0900

    Deploy: 2026-01-11 15:51

--- assets/index-DNmLk3QE.js
+++ assets/index-DNmLk3QE.js
@@ -65,23 +65,19 @@ tags:
   - list
 ---
 
-본 리스트는 예고없이 수정 될 수 있습니다
+**본 리스트는 예고없이 수정 될 수 있습니다**
 
 ## [정준일 - 어떻게 사랑이 그래요(이승환)](https://www.youtube.com/watch?v=fAAM6Pq1kjU&list=RDfAAM6Pq1kjU&start_radio=1)
 
 ## [조째즈 - 모르시나요(다비치)](https://www.youtube.com/watch?v=FRxcsTxSI18&list=RDFRxcsTxSI18&start_radio=1)
 
-## KCM - 버릇처럼 셋을 센다
-https://www.youtube.com/watch?v=X3FSotPWi7s&list=RDX3FSotPWi7s&start_radio=1
+## [KCM - 버릇처럼 셋을 센다](https://www.youtube.com/watch?v=X3FSotPWi7s&list=RDX3FSotPWi7s&start_radio=1)
 
-## 카더가든 - 나무
-https://www.youtube.com/watch?v=cHkDZ1ekB9U&list=RDcHkDZ1ekB9U&start_radio=1
+## [카더가든 - 나무](https://www.youtube.com/watch?v=cHkDZ1ekB9U&list=RDcHkDZ1ekB9U&start_radio=1)
 
-## 린 - 시간을 거슬러
-https://www.youtube.com/watch?v=E4asvdt9B5E&list=RDE4asvdt9B5E&start_radio=1
+## [린 - 시간을 거슬러](https://www.youtube.com/watch?v=E4asvdt9B5E&list=RDE4asvdt9B5E&start_radio=1)
 
-## 케이윌 - 시간을 거슬러
-https://www.youtube.com/watch?v=yXz47iRuD0Y&list=RDyXz47iRuD0Y&start_radio=1
+## [케이윌 - 시간을 거슬러](https://www.youtube.com/watch?v=yXz47iRuD0Y&list=RDyXz47iRuD0Y&start_radio=1)
 
 `,ES=`---
 title: emoji tesy
